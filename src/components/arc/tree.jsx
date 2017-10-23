bdc4acaeb8261b561073b838dd858cf11e0784c1
Andrew Wittrock <andy@wittrock.us>
2017-10-23 15:18:04 -0500

    Fix issues with arc tree

--- src/components/arc/tree.jsx
+++ src/components/arc/tree.jsx
@@ -3,34 +3,18 @@
  */
 
 import React from "react";
-import {Row, Col, ListGroup, ListGroupItem} from "react-bootstrap";
 import {TreeNode} from 'atp-tree';
 
 const ArcTree = props =>
-    <div>
-        {props.arcs.map(arc =>
+    <ul style={{listStyleType: "none"}}>
+        {props.arcIds.map(arcId =>
             <TreeNode
-                key={arc.id}
-                id={arc.id}
-                getObject={props.getObject}
-                getContent={arc => arc.name}
-                getId={arc => arc.id}
-                getChildren={props.getChildren}
-                sorter={props.sorter}
+                key={arcId}
+                id={arcId}
+                parentNodeId="arcTree"
+                {...props}
             />
         )}
-    </div>;
-
-// const ArcTree = props =>
-//     <ListGroup style={{marginTop: 0, marginBottom: 0}}>
-//         {props.arcs.filter(arc => arc.parentArcId == props.parentArcId).map(arc =>
-//             <ListGroupItem key={arc.id} style={{paddingLeft: 0, paddingRight: 0, paddingBottom: 0, borderLeft: "none", borderRight: "none", borderBottom: "none"}}>
-//                 <div style={{marginLeft: props.offset + "px", marginBottom: "8px"}}><i className="fa fa-folder-open"/> {arc.name}</div>
-//                 {props.arcs.filter(child => child.parentArcId == arc.parentArcId).length > 0 &&
-//                     <ArcTree arcs={props.arcs} parentArcId={arc.id} onClick={props.onClick} onMove={props.onMove} offset={props.offset + 32}/>
-//                 }
-//             </ListGroupItem>
-//         )}
-//     </ListGroup>;
+    </ul>;
 
 export default ArcTree;
--- src/containers/arc/tree.js
+++ src/containers/arc/tree.js
@@ -8,8 +8,10 @@ import {Arc} from "../../reducer/arc";
 
 export default connectWithLifecycle(
     state => ({
-        arcs: Arc().select.some(() => state, arc => !arc.parentId),
-        getObject: id => Arc().select.one(() => state, id),
+        arcIds: Arc().select.some(() => state, arc => !arc.parentId).map(arc => arc.id),
+        getObject: (state, id) => Arc().select.one(() => state, id),
+        getContent: arc => {console.log(arc); return arc.name},
+        getId: arc => arc.id,
         getChildren: (state, id) => Arc().select.some(() => state, arc => arc.parentId === id),
         sorter: (a, b) => a.sortOrder - b.sortOrder
     }),
