ab43dc1b643e77ba4f68b61a904f0a1a4c44d0c2
Andrea <andrea@wittrock.us>
2017-11-03 00:05:33 -0500

    Remove div from arc details component

--- src/components/arc/details.jsx
+++ src/components/arc/details.jsx
@@ -5,70 +5,65 @@ import {Row, Col, Panel} from 'react-bootstrap';
 import PageList from "../../containers/page/list";
 
 export default ({arc, updateArc, updateThumbnail, updateBanner, onNewPage}) =>
-    <div>
-        {arc &&
-            <div>
-                <Row>
-                    <Col xs={12}>
-                        <h1 style={{marginTop: 0}}>
-                            <InlineEdit.Text
-                                id="arc.name.edit"
-                                value={arc.name}
-                                label="Name"
-                                name="name"
-                                onSave={updateArc}
-                            />
-                        </h1>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={12}>
-                        <h3 style={{marginTop: 0}}>
-                            <InlineEdit.Text
-                                id="arc.url.edit"
-                                value={arc.url}
-                                label="Url"
-                                name="url"
-                                onSave={updateArc}
-                            />
-                        </h3>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={12} sm={6} md={4}>
-                        <MediaSelector.Image title="Thumbnail Image" mediaId={arc.thumbnailFileId} onSave={updateThumbnail}/>
-                    </Col>
-                    <Col xs={12} sm={6} md={4}>
-                        <MediaSelector.Image title="Banner Image" mediaId={arc.bannerFileId} onSave={updateBanner}/>
-                    </Col>
-                    <Col xs={12} sm={12} md={4}>
-                        <Panel header={<span><i className="fa fa-file-text-o"></i> Summary</span>}>
-                            <InlineEdit.Text
-                                id="arc.summary.edit"
-                                value={arc.summary}
-                                name="summary"
-                                onSave={updateArc}
-                            />
-                        </Panel>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={12}>
-                        <Panel header={
-                            <div>
-                                <span><i className="fa fa-files-o" /> Pages</span>
-                                <div style={{float: "right"}}>
-                                    <NewMediaForm bsSize="xsmall" showText={true} onUpload={onNewPage}/>
-                                </div>
+    arc
+        ? <div>
+            <Row>
+                <Col xs={12}>
+                    <h1 style={{marginTop: 0}}>
+                        <InlineEdit.Text
+                            id="arc.name.edit"
+                            value={arc.name}
+                            label="Name"
+                            name="name"
+                            onSave={updateArc}
+                        />
+                    </h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12}>
+                    <h3 style={{marginTop: 0}}>
+                        <InlineEdit.Text
+                            id="arc.url.edit"
+                            value={arc.url}
+                            label="Url"
+                            name="url"
+                            onSave={updateArc}
+                        />
+                    </h3>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12} sm={6} md={4}>
+                    <MediaSelector.Image title="Thumbnail Image" mediaId={arc.thumbnailFileId} onSave={updateThumbnail}/>
+                </Col>
+                <Col xs={12} sm={6} md={4}>
+                    <MediaSelector.Image title="Banner Image" mediaId={arc.bannerFileId} onSave={updateBanner}/>
+                </Col>
+                <Col xs={12} sm={12} md={4}>
+                    <Panel header={<span><i className="fa fa-file-text-o"></i> Summary</span>}>
+                        <InlineEdit.Text
+                            id="arc.summary.edit"
+                            value={arc.summary}
+                            name="summary"
+                            onSave={updateArc}
+                        />
+                    </Panel>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12}>
+                    <Panel header={
+                        <div>
+                            <span><i className="fa fa-files-o" /> Pages</span>
+                            <div style={{float: "right"}}>
+                                <NewMediaForm bsSize="xsmall" showText={true} onUpload={onNewPage}/>
                             </div>
-                        }>
-                            <PageList filter={page => page.arcId == arc.id} />
-                        </Panel>
-                    </Col>
-                </Row>
-            </div>
-        }
-        {!arc &&
-            <h1>No arc selected...</h1>
-        }
-    </div>;
+                        </div>
+                    }>
+                        <PageList filter={page => page.arcId == arc.id} />
+                    </Panel>
+                </Col>
+            </Row>
+          </div>
+        : <h1>No arc selected...</h1>;
