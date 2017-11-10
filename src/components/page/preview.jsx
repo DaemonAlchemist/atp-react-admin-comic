1be435a1affc306b1449464d555b891502c1141b
Andrea <andrea@wittrock.us>
2017-11-10 01:42:20 -0600

    Add page list drop targets

--- src/components/page/preview.jsx
+++ src/components/page/preview.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import {Image} from 'atp-media';
-import {DragSource} from 'atp-dnd';
+import {Draggable, DropTarget, Active} from 'atp-dnd';
 
 export const pageDragType = 'comic-page';
 
 export default ({page, dragSource}) =>
-    <DragSource type={pageDragType} id={page.id}>
-        <Image mediaId={page.imageId} />
-        Page {page.id}
-    </DragSource>;
+    <div>
+        <Draggable type={pageDragType} id={page.id} style={{position: "relative"}}>
+            <Image mediaId={page.imageId} />
+            <DropTarget
+                action="after"
+                accepts={[pageDragType]}
+                id={page.id}
+                style={{
+                    position: "absolute",
+                    right: "-40px",
+                    width: "64px",
+                    height: "100%",
+                    top: 0,
+                    zIndex: 999,
+                    paddingLeft: "24px"
+                }}
+            >
+                <Active><i className="fa fa-arrow-down"/></Active>
+            </DropTarget>
+            Page {page.id}
+        </Draggable>
+    </div>;
