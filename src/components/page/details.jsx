838c5c845fd16a6498caa7e083270745b34cc756
Andrew Wittrock <andy@wittrock.us>
2017-12-10 09:10:51 -0600

    Add name inline edit to page details component

--- src/components/page/details.jsx
+++ src/components/page/details.jsx
@@ -6,6 +6,17 @@ import {Row, Col} from 'react-bootstrap';
 export default ({page, updatePage, updateImage}) =>
     <Row>
         <Col xs={12} sm={6}>
-            <MediaSelector.Image title="Page Image" imageId={page.imageId} onSave={updateImage}/>
+            <MediaSelector.Image title="Page Image" imageId={page.imageId} onSave={updateImage} width={false} height={false} />
+        </Col>
+        <Col xs={12} sm={6}>
+            <h1 style={{marginTop: 0}}>
+                <InlineEdit.Text
+                    id={"page.name.edit" + page.id}
+                    value={page.name}
+                    label="Name"
+                    name="name"
+                    onSave={updatePage}
+                />
+            </h1>
         </Col>
     </Row>;
