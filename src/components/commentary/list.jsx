44d98705852022498cdfa872763d9fa4974c1758
Andrew Wittrock <andy@wittrock.us>
2017-12-13 01:01:11 -0600

    Begin implementing draggable commentary list rows

--- src/components/commentary/list.jsx
+++ src/components/commentary/list.jsx
@@ -4,6 +4,9 @@ import {PageLinkFull} from "../links";
 import {UserLinkFull} from "atp-uac";
 import {Panel, Table} from 'react-bootstrap';
 import {InlineEdit} from 'atp-inline-edit';
+import {Draggable, DropTarget, Active} from 'atp-dnd';
+
+export const commentaryDragType = 'comic-commentary';
 
 export default ({comments, updateComment}) =>
     <Panel  header={<span><i className="fa fa-comment"></i> Commentary</span>}>
@@ -19,7 +22,7 @@ export default ({comments, updateComment}) =>
             </thead>
             <tbody>
             {comments.map(comment =>
-                <tr key={comment.id}>
+                <Draggable type={commentaryDragType} id={comment.id} component="tr" key={comment.id}>
                     <td><i className="fa fa-bars" /></td>
                     <td><UserLinkFull userId={comment.userId} /></td>
                     <td><PageLinkFull pageId={comment.pageId} /></td>
@@ -32,7 +35,7 @@ export default ({comments, updateComment}) =>
                         rows={3}
                     /></td>
                     <td className="text-danger"><i className="fa fa-trash" /></td>
-                </tr>
+                </Draggable>
             )}
             </tbody>
         </Table>
