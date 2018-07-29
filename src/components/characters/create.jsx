cb96164cf2ba1175f8863a84b41109c74f8e6ffe
Andrew Wittrock <andy@wittrock.us>
2018-07-28 22:53:38 -0500

    Add permission wrappers to character list

--- src/components/characters/create.jsx
+++ src/components/characters/create.jsx
@@ -3,9 +3,12 @@ import React from "react";
 import {Field} from "redux-form";
 import {Button} from "react-bootstrap";
 import {Icon} from 'react-font-awesome-5';
+import {HasPermission} from "atp-uac";
 
 export default props =>
-    <form onSubmit={props.handleSubmit}>
-        <Field name="name" placeholder="Character name" component="input" className="form-control"/>
-        <Button bsStyle="link" style={{position: "absolute", top: "10px", right: "16px", color: "#000"}} type="submit"><Icon.Plus /> Create</Button>
-    </form>;
+    <HasPermission permissions={["comic.character.create"]}>
+        <form onSubmit={props.handleSubmit}>
+            <Field name="name" placeholder="Character name" component="input" className="form-control"/>
+            <Button bsStyle="link" style={{position: "absolute", top: "10px", right: "16px", color: "#000"}} type="submit"><Icon.Plus /> Create</Button>
+        </form>
+    </HasPermission>;
--- src/components/characters/dashboard.jsx
+++ src/components/characters/dashboard.jsx
@@ -7,6 +7,7 @@ import CharacterDetails from "../../containers/characters/details";
 import {o} from 'atp-sugar';
 import {Draggable, DropTargets} from 'atp-dnd';
 import {Icon} from 'react-font-awesome-5';
+import {HasPermission} from "atp-uac";
 
 export const characterDragType = 'comic-character';
 
@@ -15,25 +16,34 @@ export default ({characters, selectedCharacterId, isSelected, onCharacterMove, o
         <Col xs={12} sm={4} md={3}>
             {characters
                 ? <ListGroup>
-                <ListGroupItem>
-                    <NewCharacterForm/>
-                </ListGroupItem>
-                <DropTargets.ListGroupItem accepts={[characterDragType]} key="insetBeforeDropTarget" id={null} action="into" onMove={onCharacterMove} />
-                {o(characters).values().sort(sortBy('sortOrder')).map(character => [
-                    <ListGroupItem key={character.id} className={isSelected(character) ? "active" : ""}>
-                        <Draggable onClick={onClickCharacter(character.id)} type={characterDragType} id={character.id} key={character.id}>
-                            <Icon.Bars /> {character.name}
-                        </Draggable>
-                    </ListGroupItem>,
-                    <DropTargets.ListGroupItem
-                        key={"characterDropTarget" + character.id}
-                        id={character.id}
-                        accepts={[characterDragType]}
-                        action="after"
-                        onMove={onCharacterMove}
-                    />
-                ])}
-            </ListGroup>
+                    <HasPermission permissions={["comic.character.create"]}>
+                        <ListGroupItem>
+                            <NewCharacterForm/>
+                        </ListGroupItem>
+                        <DropTargets.ListGroupItem accepts={[characterDragType]} key="insetBeforeDropTarget" id={null} action="into" onMove={onCharacterMove} />
+                    </HasPermission>
+                    {o(characters).values().sort(sortBy('sortOrder')).map(character => [
+                        <HasPermission yes permissions={["comic.character.create"]}>
+                            <ListGroupItem key={character.id} className={isSelected(character) ? "active" : ""}>
+                                <Draggable onClick={onClickCharacter(character.id)} type={characterDragType} id={character.id} key={character.id}>
+                                    <Icon.Bars /> {character.name}
+                                </Draggable>
+                            </ListGroupItem>,
+                            <DropTargets.ListGroupItem
+                                key={"characterDropTarget" + character.id}
+                                id={character.id}
+                                accepts={[characterDragType]}
+                                action="after"
+                                onMove={onCharacterMove}
+                            />
+                        </HasPermission>,
+                        <HasPermission no permissions={["comic.character.create"]}>
+                            <ListGroupItem key={character.id} className={isSelected(character) ? "active" : ""} onClick={onClickCharacter(character.id)}>
+                                {character.name}
+                            </ListGroupItem>
+                        </HasPermission>
+                    ])}
+                  </ListGroup>
                 : <span><Icon.Spinner spin /> Loading...</span>
             }
         </Col>
